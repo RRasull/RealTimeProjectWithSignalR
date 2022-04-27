adbd1a43a8174ecb742712718e0ac62e8bf025b5
RRasull <92165344+RRasull@users.noreply.github.com>
2022-04-28 00:07:13 +0400

    "feat: add ChatHub class and download SignalR lib"

--- RealTimeProjectWithSignalR/Startup.cs
+++ RealTimeProjectWithSignalR/Startup.cs
@@ -15,6 +15,7 @@ namespace RealTimeProjectWithSignalR
        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
