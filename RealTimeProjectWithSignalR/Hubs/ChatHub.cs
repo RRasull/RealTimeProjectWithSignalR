fab3e2e195994a785d2363f9535728b9ae6c54dd
RRasull <92165344+RRasull@users.noreply.github.com>
2022-04-30 15:18:33 +0400

    feat: add clients List

--- RealTimeProjectWithSignalR/Hubs/ChatHub.cs
+++ RealTimeProjectWithSignalR/Hubs/ChatHub.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace RealTimeProjectWithSignalR.Hubs
 {
     public class ChatHub : Hub
     {
+        static List<string> clients = new List<string>(); 
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
@@ -13,11 +15,15 @@ namespace RealTimeProjectWithSignalR.Hubs
 
         public override async Task OnConnectedAsync()
         {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients",clients);
             await Clients.All.SendAsync("clientJoined",Context.ConnectionId);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
             await Clients.All.SendAsync("clientLeaved", Context.ConnectionId);
 
         }
